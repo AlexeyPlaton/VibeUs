0b8bc9d0d280ee5688d0748fd0a07f23eefba424
AlexeyPlaton <133057036+AlexeyPlaton@users.noreply.github.com>
2026-09-03 20:53:30 +0300

    feat(widget): bundle responsive shell into standalone widget

--- openspec-web/src/widget.tsx
+++ openspec-web/src/widget.tsx
@@ -4,6 +4,7 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n/config';
 import { VibusWidgetUI } from './components/VibusWidgetUI';
 import './index.css';
+import './widget-responsive.css';
 
 function consumeUrlCapability(): string {
   if (typeof window === 'undefined') return '';
