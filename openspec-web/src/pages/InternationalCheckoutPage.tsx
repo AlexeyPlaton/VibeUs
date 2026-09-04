95839766530a9d1f8911493537ce4c11f4c49833
AlexeyPlaton <133057036+AlexeyPlaton@users.noreply.github.com>
2026-09-04 06:45:16 +0300

    Show EEA and UK countries before server-side billing decision

--- openspec-web/src/pages/InternationalCheckoutPage.tsx
+++ openspec-web/src/pages/InternationalCheckoutPage.tsx
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
 import { LanguageSwitcher } from '../components/LanguageSwitcher';
 import { tr } from '../i18n/config';
 
-const ISO2 = `AF AL DZ AS AD AO AI AQ AG AR AM AW AU AZ BS BH BD BB BY BZ BJ BM BT BO BQ BA BW BV BR IO BN BF BI CV KH CM CA KY CF TD CL CN CX CC CO KM CG CD CK CR CI CU CW DJ DM DO EC EG SV GQ ER SZ ET FK FO FJ GF PF TF GA GM GE GH GI GL GD GP GU GT GG GN GW GY HT HM VA HN HK IN ID IR IQ IM IL JM JP JE JO KZ KE KI KP KR KW KG LA LB LS LR LY MO MG MW MY MV ML MH MQ MR MU YT MX FM MD MC MN ME MS MA MZ MM NA NR NP NC NZ NI NE NG NU NF MK MP OM PK PW PS PA PG PY PE PH PN PR QA RE RU RW BL SH KN LC MF PM VC WS SM ST SA SN RS SC SL SG SX SB SO ZA GS SS LK SD SR SJ CH SY TW TJ TZ TH TL TG TK TO TT TN TR TM TC TV UG UA AE US UM UY UZ VU VE VN VG VI WF EH YE ZM ZW`.split(' ');
+// Keep the selector geographically complete. Availability is a server-owned
+// billing/compliance decision: users in a currently blocked country should be
+// able to identify their real billing country and receive an explicit reason,
+// rather than discovering that their country has simply disappeared from UI.
+const ISO2 = `AF AL DZ AS AD AO AI AQ AG AR AM AW AU AT AZ BS BH BD BB BY BE BZ BJ BM BT BO BQ BA BW BV BR IO BN BG BF BI CV KH CM CA KY CF TD CL CN CX CC CO KM CG CD CK CR CI HR CU CW CY CZ DK DJ DM DO EC EG SV GQ ER EE SZ ET FK FO FJ FI FR GF PF TF GA GM GE DE GH GI GR GL GD GP GU GT GG GN GW GY HT HM VA HN HK HU IS IN ID IR IQ IE IM IL IT JM JP JE JO KZ KE KI KP KR KW KG LA LV LB LS LR LY LI LT LU MO MG MW MY MV ML MT MH MQ MR MU YT MX FM MD MC MN ME MS MA MZ MM NA NR NP NL NC NZ NI NE NG NU NF MK MP NO OM PK PW PS PA PG PY PE PH PN PL PT PR QA RE RO RU RW BL SH KN LC MF PM VC WS SM ST SA SN RS SC SL SG SX SK SI SB SO ZA GS SS ES LK SD SR SJ SE CH SY TW TJ TZ TH TL TG TK TO TT TN TR TM TC TV UG UA AE GB US UM UY UZ VU VE VN VG VI WF EH YE ZM ZW`.split(' ');
 
 function apiUrl() {
   return window.location.origin.includes('localhost') ? 'http://localhost:8000' : window.location.origin;
