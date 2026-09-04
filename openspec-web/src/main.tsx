4e6d9735781c99d7fc4e3cf1f383b0bbf1ab035e
AlexeyPlaton <133057036+AlexeyPlaton@users.noreply.github.com>
2026-09-04 07:10:59 +0300

    Load enterprise board UX layer in app build

--- openspec-web/src/main.tsx
+++ openspec-web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import './widget-responsive.css';
 import './enterprise-board.css';
+import './enterprise-board-ux.css';
 import './enterprise-dialogs.css';
 import App from './App';
 import './pages/landingpage.v8.css';
