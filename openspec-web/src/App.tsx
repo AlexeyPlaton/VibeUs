8afc7fcd22ab3e1129402a4ea8428849bfbebcbc
AlexeyPlaton <133057036+AlexeyPlaton@users.noreply.github.com>
2026-09-05 09:05:05 +0300

    Remove founder console routes from public web runtime

--- openspec-web/src/App.tsx
+++ openspec-web/src/App.tsx
@@ -6,11 +6,6 @@ import { InternationalCheckoutPage } from './pages/InternationalCheckoutPage';
 import { AIOrchestrationPage } from './pages/AIOrchestrationPage';
 import { DeliveryIntegrationsPage } from './pages/DeliveryIntegrationsPage';
 import { GitHubAppCallbackPage } from './pages/GitHubAppCallbackPage';
-import { ControlCenterPage } from './pages/ControlCenterPage';
-import { ProductRadarPage } from './pages/ProductRadarPage';
-import { FounderWorkbenchPage } from './pages/FounderWorkbenchPage';
-import { FounderGrowthStrategyPage } from './pages/FounderGrowthStrategyPage';
-import { FounderControlShell } from './components/FounderControlShell';
 import { LegalPage } from './pages/legalpage';
 import { EnterpriseDashboardFrame } from './components/EnterpriseDashboardFrame';
 import './index.css';
@@ -25,10 +20,6 @@ function App() {
         <Route path="/app/ai/:projectSlug" element={<EnterpriseDashboardFrame><AIOrchestrationPage /></EnterpriseDashboardFrame>} />
         <Route path="/app/integrations/:projectSlug" element={<EnterpriseDashboardFrame><DeliveryIntegrationsPage /></EnterpriseDashboardFrame>} />
         <Route path="/app/integrations/github/callback" element={<EnterpriseDashboardFrame><GitHubAppCallbackPage /></EnterpriseDashboardFrame>} />
-        <Route path="/control" element={<FounderControlShell><ProductRadarPage /></FounderControlShell>} />
-        <Route path="/control/strategy" element={<FounderControlShell><FounderGrowthStrategyPage /></FounderControlShell>} />
-        <Route path="/control/workbench" element={<FounderControlShell><FounderWorkbenchPage /></FounderControlShell>} />
-        <Route path="/control/ops" element={<FounderControlShell><ControlCenterPage /></FounderControlShell>} />
         <Route path="/billing/international" element={<InternationalCheckoutPage />} />
         <Route path="/legal/:doc" element={<LegalPage />} />
       </Routes>
